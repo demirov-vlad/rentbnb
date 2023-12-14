9d1cc6088d122dc9cd537a822884e5a78fd9b0cf
Vlad <demirov.vlad@gmail.com>
2023-12-14 16:57:33 +0200

    final fixes

--- app/components/navbar/Logo.tsx
+++ app/components/navbar/Logo.tsx
@@ -1,23 +1,24 @@
 "use client";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
-import Container from "@/app/components/Container";
 
 const Logo = () => {
   const router = useRouter();
   return (
     <div
       onClick={() => router.push("/")}
-      className="flex flex-row items-center gap-2 font-semibold text-rose-500 text-2xl cursor-pointer"
+      className="flex flex-row items-center gap-2"
     >
       <Image
         src="/images/logo.png"
         alt="Logo"
-        height="30"
-        width="30"
-        className="hidden md:block cursor-pointer"
+        height="45"
+        width="45"
+        className="block cursor-pointer"
       />
-      rentbnb
+      <p className="hidden md:block font-semibold text-rose-500 text-2xl cursor-pointer">
+        rentbnb
+      </p>
     </div>
   );
 };
